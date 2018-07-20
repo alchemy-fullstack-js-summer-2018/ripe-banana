d47bea70020fa20d71dd627bcdf365672018450a
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-07-20 11:40:55 -0700

    Rewrite data for test

--- test/e2e/studios.test.js
+++ test/e2e/studios.test.js
@@ -7,79 +7,87 @@ const checkOk = res => {
     return res;
 };
 
+function save(studio) {
+    return request
+        .post('/api/studios')
+        .send(studio)
+        .then(checkOk)
+        .then(({ body }) => body);
+}
+
+const makeSimple = (studio) => {
+    const simple = {
+        _id: studio._id,
+        name: studio.name
+    };
+    return simple;
+};
+
+const makeWithoutVersion = (studio) => {
+    const noVersion = {
+        _id: studio._id,
+        name: studio.name,
+        address: studio.address
+    };
+    return noVersion;
+};
+
+let warnerStudios;
+let netflixStudios;
+
+const warner = {
+    name: 'Warner Bros',
+    address: {
+        city: 'Bankbur',
+        state: 'CA',
+        country: 'United States'
+    }
+};
+
+const netflix = {
+    name: 'Netflix',
+    address: {
+        city: 'Los Angeles',
+        state: 'CA',
+        country: 'United States'
+    }
+};
+
 describe('Studios API', () => {
+
     beforeEach(() => dropCollection('studios'));
 
-    function save(studio) {
-        return request
-            .post('/api/studios')
-            .send(studio)
-            .then(checkOk)
-            .then(({ body }) => body);
-    }
-
-    const makeSimple = (studio) => {
-        const simple = {
-            _id: studio._id,
-            name: studio.name
-        };
-        return simple;
-    };
-
-    const makeWithoutVersion = (studio) => {
-        const noVersion = {
-            _id: studio._id,
-            name: studio.name,
-            address: studio.address
-        };
-        return noVersion;
-    };
-
-    let warner;
+    beforeEach(() => {
+        return save(warner)
+            .then(data => warnerStudios = data);
+    });
 
     beforeEach(() => {
-        return save({
-            name: 'Warner Bros',
-            address: {
-                city: 'Bankbur',
-                state: 'CA',
-                country: 'United States'
-            }
-        })
-            .then(data => warner = data);
+        return save(netflix)
+            .then(data => netflixStudios = data);
     });
 
     it('saves a studio to the database', () => {
-        assert.isOk(warner._id);
+        assert.isOk(warnerStudios._id);
+        assert.isOk(netflixStudios._id);
     });
 
     it('gets all studios', () => {
-        let netflix;
-        return save({
-            name: 'Netflix',
-            address: {
-                city: 'Los Angeles',
-                state: 'CA',
-                country: 'United States'
-            }
-        })
-            .then(data => {
-                netflix = data;
-                return request.get('/api/studios');
-            })
+        return request
+            .get('/api/studios')
             .then(({ body }) => {
                 assert.deepEqual(body, [
-                    makeSimple(warner),
-                    makeSimple(netflix)
+                    makeSimple(warnerStudios),
+                    makeSimple(netflixStudios)
                 ]);
             });
     });
 
     it('gets a studio by ID', () => {
         return request
-            .get(`/api/studios/${warner._id}`)
+            .get(`/api/studios/${warnerStudios._id}`)
             .then(({ body }) => {
-                assert.deepEqual(body, makeWithoutVersion(warner));
+                assert.deepEqual(body, makeWithoutVersion(warnerStudios));
             });
     });
 });
