d3b4caa1cd9a0725dc72ccf34eddba36f429bc1a
Easton Gorishek <ieaston26@gmail.com>
2018-07-20 09:57:27 -0700

    actors e2e tdd success

---
+++ test/e2e/actors.test.js
@@ -0,0 +1,56 @@
+const { assert } = require('chai');
+const request = require('./request');
+const { dropCollection } = require('./db');
+const { checkOk } = request;
+
+describe('Actor API', () => {
+
+    beforeEach(() => dropCollection('actors'));
+
+    function save(actor) {
+        return request
+            .post('/api/actors')
+            .send(actor)
+            .then(checkOk)
+            .then(({ body }) => body);
+    }
+
+    let actor;
+
+    beforeEach(() => {
+        return save({
+            name: 'Easton',
+            dob: new Date(1990, 10, 19),
+            pob: 'Phoenix'
+
+        })
+            .then(data => {
+                actor = data;
+            });
+    });
+
+    it('saves a actor', () => {
+        assert.isOk(actor._id);
+    });
+
+    it('gets all actors', () => {
+        let mark;
+        return save({ name: 'Mark' })
+            .then(_mark => {
+                mark = _mark;
+                return request.get('/api/actors');  
+            })
+            .then(checkOk)
+            .then(({ body }) => {
+                assert.deepEqual(body, [actor, mark]);
+            });
+    });
+
+    it('gets a actor by id', () => {
+        return request
+            .get(`/api/actors/${actor._id}`)
+            .then(({ body }) => {
+                assert.deepEqual(body, actor);
+            });
+    });
+});
