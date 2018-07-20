a00613f1c85ba02010d932198c1d47c2acef2316
Easton Gorishek <ieaston26@gmail.com>
2018-07-19 17:45:09 -0700

    working on first e2e tdd

---
+++ test/e2e/studios.test.js
@@ -0,0 +1,38 @@
+const { assert } = require('chai');
+const request = require('./request');
+const { dropCollection } = require('./db');
+//const { checkOk } = request;
+
+describe('Studio API', () => {
+
+    // beforeEach(() => dropCollection('studios'));
+
+    function save(studio) {
+        return request
+            .post('/api/studios')
+            .send(studio)
+            //.then(checkOk)
+            .then(({ body }) => body);
+
+    }
+
+    let studio;
+
+    beforeEach(() => {
+        return save({
+            name: 'The Kick Ass Studio',
+            address: {
+                city: 'Portland',
+                state: 'Oregon',
+                country: 'USA'
+            }
+        })
+            .then(data => {
+                studio = data;
+            });
+    });
+
+    it('saves a studio', () => {
+        assert.isOk(studio._id);
+    });
+});
