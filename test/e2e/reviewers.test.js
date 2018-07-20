6d63615968a65dab0cff1ab0782bc7a84b7c527a
Easton Gorishek <ieaston26@gmail.com>
2018-07-20 10:07:41 -0700

    reviewers unit and e2e tdd success

---
+++ test/e2e/reviewers.test.js
@@ -0,0 +1,57 @@
+const { assert } = require('chai');
+const request = require('./request');
+const { dropCollection } = require('./db');
+const { checkOk } = request;
+
+describe('Reviewer API', () => {
+
+    beforeEach(() => dropCollection('reviewers'));
+
+    function save(reviewer) {
+        return request
+            .post('/api/reviewers')
+            .send(reviewer)
+            .then(checkOk)
+            .then(({ body }) => body);
+    }
+
+    let reviewer;
+
+    beforeEach(() => {
+        return save({
+            name: 'Bobby',
+            company: 'Unemployed'
+        })
+            .then(data => {
+                reviewer = data;
+            });
+    });
+
+    it('saves a reviewer', () => {
+        assert.isOk(reviewer._id);
+    });
+
+    it('gets all reviewers', () => {
+        let carrie;
+        return save({ 
+            name: 'carrie',
+            company: 'Student'
+        })
+            .then(_carrie => {
+                carrie = _carrie;
+                return request.get('/api/reviewers');  
+            })
+            .then(checkOk)
+            .then(({ body }) => {
+                assert.deepEqual(body, [reviewer, carrie]);
+            });
+    });
+
+    it('gets a reviewer by id', () => {
+        return request
+            .get(`/api/reviewers/${reviewer._id}`)
+            .then(({ body }) => {
+                assert.deepEqual(body, reviewer);
+            });
+    });
+});
---
+++ test/unit/reviewer.test.js
@@ -0,0 +1,26 @@
+const { assert } = require('chai');
+const { getErrors } = require('./helpers');
+const Reviewer = require('../../lib/models/reviewer');
+
+describe('Reviewer model', () => {
+    
+    it('validates a good model', () => {
+        const data = {
+            name: 'Bobby',
+            company: 'Unemployed'
+        };
+
+        const reviewer = new Reviewer(data);
+        const json = reviewer.toJSON();
+        delete json._id;
+        assert.deepEqual(json, data);
+        assert.isUndefined(reviewer.validateSync());
+    });
+
+    it('validates that a name and compnay is required', () => {
+        const reviewer = new Reviewer({});
+        const errors = getErrors(reviewer.validateSync(), 2);
+        assert.equal(errors.name.kind, 'required');
+        assert.equal(errors.company.kind, 'required');
+    });
+});
