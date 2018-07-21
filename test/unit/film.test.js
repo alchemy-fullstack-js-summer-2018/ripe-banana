ecf5e5eb7eeb8be66c82b690589e153952f5fef2
rbtprograms <rbt61690@gmail.com>
2018-07-20 17:34:07 -0700

    film unit tests done

--- test/unit/film.test.js
+++ test/unit/film.test.js
@@ -1,6 +1,6 @@
 const { assert } = require('chai');
 const { Types } = require('mongoose');
-// const { getErrors } = require('./helpers');
+const { getErrors } = require('./helpers');
 const Film = require('../../lib/models/film');
 // const Actor = require('../../lib/models/actor');
 // const Studio = require('../../lib/models/studio');
@@ -22,5 +22,13 @@ describe('Film model', () => {
         delete json._id;
         delete json.cast[0]._id;
         assert.deepEqual(json, data);
+    });
+
+    it('validates required fields', () => {
+        const film = new Film({});
+        const errors = getErrors(film.validateSync(), 3);
+        assert.equal(errors.title.kind, 'required');
+        assert.equal(errors.studio.kind, 'required');
+        assert.equal(errors.released.kind, 'required');
     });
 });
