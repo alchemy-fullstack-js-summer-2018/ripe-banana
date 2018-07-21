dd23fbedaac92ec9d989ab10a17587ffcd5ed9c2
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-07-21 15:24:27 -0700

    Implement route for saving a film and test

--- test/e2e/films.test.js
+++ test/e2e/films.test.js
@@ -1,40 +1,67 @@
 const { assert } = require('chai');
 const request = require('./request');
 const { dropCollection } = require('./db');
-const actor = require('../../lib/models/actor');
-const studio = require('../../lib/models/studio');
+const { checkOk } = request;
 
-const checkOk = res => {
-    assert.equal(res.status, 200, 'expected http 200 status code');
-    return res;
+function save(film) {
+    return request
+        .post('/api/films')
+        .send(film)
+        .then(checkOk)
+        .then(({ body }) => body);
+}
+
+let inceptionFilm;
+/* eslint-disable-next-line  */
+let legendaryStudio;
+/* eslint-disable-next-line  */
+let leoActor;
+
+const legendary = {
+    name: 'Legendary',
+    address: {
+        city: 'Santa Monica',
+        state: 'CA',
+        country: 'United States'
+    }
+};
+
+const leo = { 
+    name:'Leonardo DiCaprio',
+    dob: new Date('1980-11-12'),
+    pob: 'Portland, OR'
 };
 
 describe.only('Films API', () => {
 
     beforeEach(() => dropCollection('films'));
-
-    function save(film) {
-        return request
-            .post('/api/films')
-            .send(film)
-            .then(checkOk)
-            .then(({ body }) => body);
-    }
-
-    let inceptionFilm;
     
-    const inception = {
-        title: 'Inception',
-        studio: studio._id,
-        released: 2010,
-        cast: [{
-            role: 'Cobb',
-            actor: actor._id
-        }]
-    };
+    beforeEach(() => {
+        return request
+            .post('/api/studios')
+            .send(legendary)
+            .then(checkOk)
+            .then(({ body }) => legendaryStudio = body);
+    });
 
     beforeEach(() => {
-        return save(inception)
+        return request
+            .post('/api/actors')
+            .send(leo)
+            .then(checkOk)
+            .then(({ body }) => leoActor = body);
+    });
+
+    beforeEach(() => {
+        return save({
+            title: 'Inception',
+            studio: legendaryStudio._id,
+            released: 2010,
+            cast: [{
+                role: 'Cobb',
+                actor: leoActor._id
+            }]
+        })
             .then(data => inceptionFilm = data);
     });
 
@@ -42,9 +69,9 @@ describe.only('Films API', () => {
         assert.isOk(inceptionFilm._id);
     });
 
-    xit('gets all actors from the db', () => {
+    xit('gets all films from the db', () => {
         return request
-            .get('/api/actors')
+            .get('/api/films')
             .then(checkOk)
             .then(({ body }) => {
                 assert.deepEqual(body, inceptionFilm);
