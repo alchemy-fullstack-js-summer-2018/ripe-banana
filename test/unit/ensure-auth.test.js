068efb692514a87c1ede883cd01f7f4ddb1a459c
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-07-25 11:03:24 -0700

    Add ensure auth unit tests

--- test/unit/ensure-auth.test.js
+++ test/unit/ensure-auth.test.js
@@ -1,1 +1,41 @@
-const { assert } = require('chai')
+const { assert } = require('chai');
+const createEnsureAuth = require('../../lib/util/ensure-auth');
+const tokenService = require('../../lib/util/token-service');
+require('dotenv').config();
+
+describe.only('Ensure auth middleware', () => {
+    const user = { _id: 123 };
+    let token = '';
+    beforeEach(() => {
+        return tokenService.sign(user)
+            .then(t => token = t);
+    });
+
+    const ensureAuth = createEnsureAuth();
+
+    it('adds a payload as req.user on success', done => {
+        const req = {
+            get(header) {
+                if(header === 'Authorization') return token;
+            }
+        };
+
+        const next = () => {
+            assert.equal(req.user.id, user._id);
+            done();
+        };
+        ensureAuth(req, null, next);
+    });
+
+    it('calls next with error when token is bad', done => {
+        const req = {
+            get() { return 'bad-token'; }
+        };
+
+        const next = err => {
+            assert.equal(err.code, 401);
+            done();
+        };
+        ensureAuth(req, null, next);
+    });
+});
