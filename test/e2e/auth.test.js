cfe7626362e17ef8f69c5f67cdcefcbe1a385c34
Mario Quintana <pyrros2000@gmail.com>
2018-07-25 08:37:38 -0700

    first auth-test for signing up a user passes

--- test/e2e/auth.test.js
+++ test/e2e/auth.test.js
@@ -4,7 +4,7 @@ const { dropCollection } = require('./db');
 
 const { checkOk } = request;
 
-describe('Auth API', () => {
+describe.only('Auth API', () => {
 
     beforeEach(() => dropCollection('users'));
 
