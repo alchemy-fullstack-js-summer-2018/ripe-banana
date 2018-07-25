ebd51b034cb65383c210bdb6c40c4687d811598f
Easton Gorishek <ieaston26@gmail.com>
2018-07-24 17:35:14 -0700

    validates a good reviewer/user model

--- test/unit/reviewer.test.js
+++ test/unit/reviewer.test.js
@@ -7,20 +7,38 @@ describe('Reviewer model', () => {
     it('validates a good model', () => {
         const data = {
             name: 'Bobby',
-            company: 'Unemployed'
+            company: 'Unemployed',
+            email: 'easton@portland.com',
+            password: 'adamngoodone',
+            roles: []
         };
 
         const reviewer = new Reviewer(data);
-        const json = reviewer.toJSON();
-        delete json._id;
-        assert.deepEqual(json, data);
+
+        assert.equal(reviewer.email, data.email);
+        assert.isUndefined(reviewer.password, 'password should not be set');
+
+        reviewer.generateHash(data.password);
+        
+        assert.isDefined(reviewer.hash, 'hash is defined');
+        assert.notEqual(reviewer.hash, data.password, 'hash is not the same as password.');
+        assert.isDefined(reviewer.hash, 'hash is defined');
+        assert.notEqual(reviewer.hash, data.password, 'hash does not equal password');
+        
+        //const json = reviewer.toJSON();
+        //delete json._id;
+        //assert.deepEqual(json, data);
         assert.isUndefined(reviewer.validateSync());
+        assert.isTrue(reviewer.comparePassword(data.password), 'compare good password');
+        assert.isFalse(reviewer.comparePassword('bad password'), 'compare bad password');
     });
 
-    it('validates that a name and company is required', () => {
+    it('validates required fields (name, company, email, hash)', () => {
         const reviewer = new Reviewer({});
-        const errors = getErrors(reviewer.validateSync(), 2);
+        const errors = getErrors(reviewer.validateSync(), 4);
         assert.equal(errors.name.kind, 'required');
         assert.equal(errors.company.kind, 'required');
+        assert.equal(errors.email.kind, 'required');
+        assert.equal(errors.hash.kind, 'required');
     });
 });
