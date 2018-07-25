7ccb77298975c88f4b0f6f167533fe16f1e6174d
Mark Myers <markalope@gmail.com>
2018-07-25 16:08:02 -0700

    updating reviewers route to fix required fields errors

--- test/e2e/auth.test.js
+++ test/e2e/auth.test.js
@@ -4,7 +4,7 @@ const { dropCollection } = require('./db');
 
 const { checkOk } = request;
 
-describe.skip('Auth API', () => {
+describe('Auth API', () => {
     
     beforeEach(() => dropCollection('users'));
 
