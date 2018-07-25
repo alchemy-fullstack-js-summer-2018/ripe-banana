448411032d272271da203564d5bd2632b7b5d3c3
Carmen V Ramos <carmenvramos@yahoo.com>
2018-07-24 18:46:27 -0700

    Good Model Password & Hash Tests Passing

--- test/unit/reviewer.test.js
+++ test/unit/reviewer.test.js
@@ -5,17 +5,27 @@ const Reviewer = require('../../lib/models/reviewer');
 
 describe('Reviewer Model', () => {
 
-    it('validates good model', () => {
+    it.only('validates good model', () => {
         const data = {
             name: 'Justin Chang',
-            company: 'The Hollywood Reporter' 
+            company: 'The Hollywood Reporter',
+            email: 'email@.email.com',
+            password: 'pwd123',
+            roles: []
         };
      
         const reviewer = new Reviewer(data);
 
-        const json = reviewer.toJSON();
-        delete json._id;
-        assert.deepEqual(json, data);
+        // const json = reviewer.toJSON();
+        // delete json._id;
+        // assert.deepEqual(json, data);
+        assert.equal(reviewer.email, data.email);
+        assert.isUndefined(reviewer.password, 'password should not be set');
+
+        reviewer.generateHash(data.password);
+        assert.isDefined(reviewer.hash, 'hash is defined');
+        assert.notEqual(reviewer.hash, data.password, 'hash not same as password');
+
     });
 
     it('validates that all fields are required', () => {
