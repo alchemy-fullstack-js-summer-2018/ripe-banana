0117ef6b20a27202f065484937f86c78b9d73358
Arthur Jen <arthurljen@gmail.com>
2018-07-24 17:18:35 -0700

    validated user model

--- test/unit/user.test.js
+++ test/unit/user.test.js
@@ -1,11 +1,15 @@
 const { assert } = require('chai');
 const User = require('../../lib/models/user');
+const { Types } = require('mongoose');
+const { getErrors } = require('./helpers');
 
-describe.only('Film model', () => {
+describe.only('User model', () => {
     it('validates good user model', () => {
         const data = {
-            email: 'marty@martypdx.com',
-            password: 'abc123'
+            email: 'arthur@gmail.com',
+            password: 'abc123',
+            reviewer: Types.ObjectId(),
+            roles: []
         };
         const user = new User(data);
         assert.equal(user.email, data.email);
@@ -18,5 +22,13 @@ describe.only('Film model', () => {
         assert.isUndefined(user.validateSync());
         assert.isTrue(user.comparePassword(data.password));
         assert.isFalse(user.comparePassword('bad password'));
+    });
+
+    it('requires email and hash', () => {
+        const user = new User({});
+        const errors = getErrors(user.validateSync(), 3);
+        assert.equal(errors.email.kind, 'required');
+        assert.equal(errors.hash.kind, 'required');
+        assert.equal(errors.reviewer.kind, 'required');
     });
 });
