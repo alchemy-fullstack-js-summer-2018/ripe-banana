64d41305895a03fbf4631690319e5db1c9372809
Carmen V Ramos <carmenvramos@yahoo.com>
2018-07-26 09:24:27 -0700

    Wrong Password, Cannot Signin With Same Email, 401 on Bad Eamil Signin Tests Passing

--- test/e2e/auth.test.js
+++ test/e2e/auth.test.js
@@ -35,4 +35,52 @@ describe('Auth API', () => {
             .then(checkOk);
     });
 
+    it('can sign in a user', () => {
+        return request
+            .post('/api/auth/signin')
+            .send({
+                email: 'justin@email.com',
+                password: 'pwd123'
+            })
+            .then(checkOk)
+            .then(({ body }) => {
+                assert.isDefined(body.token);
+            });
+    });
+
+    it('fails on wrong password', () => {
+        return request
+            .post('/api/auth/signin')
+            .send({
+                email: 'justin@email.com',
+                password: 'bad'
+            })
+            .then(res => {
+                assert.equal(res.status, 401);
+                assert.equal(res.body.error, 'Invalid email or password')
+            });
+    });
+
+    it('cannot signup with same email', () => {
+        return request
+            .post('/api/auth/signup')
+            .send({
+                email: 'justin@email.com',
+                password: 'pwd123'
+
+            })
+            .then(res => {
+                assert.equal(res.status, 400);
+                assert.equal(res.body.error, 'Email already in use');
+            });
+    });
+
+    it('Give 401 on bad email signin', () => {
+        return request
+            .post('/api/auth/signin')
+            .send({
+                email: 'notjustin@email.com',
+                password: 'pwd123'
+            });
+    });
 });
