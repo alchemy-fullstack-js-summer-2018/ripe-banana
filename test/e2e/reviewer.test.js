7f9b7a8e117031d319a5dc787b4a16ba488263fa
Ryan Manro <rmanro@gmail.com>
2018-07-27 15:39:10 -0700

    Revert "fixed env and console logs, all done"

--- test/e2e/reviewer.test.js
+++ test/e2e/reviewer.test.js
@@ -24,6 +24,7 @@ describe('Reviewer API', () => {
             .then(({ body }) => {
                 token = body.token;
                 tyrone = body.reviewer;
+                console.log('** token **', token);
             });
     });
 
